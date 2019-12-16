b6a5cd013926317e9dd24c8248ece03b600ccf3f
github username <josephtrettevik@gmail.com>
2019-12-16 15:19:55 -0800

    added an "insert node at tail of list" function to the SinglyLinkedlist class

--- singly_listed_list.js
+++ singly_listed_list.js
@@ -19,14 +19,30 @@ class SinglyLinkedList {
         }
     }
 
+    insertNodeAtTail = (data) => {
+        const newNode = new SinglyLinkedListNode(data)
+        if (this.head) {
+            let currentNode = this.head
+            while(currentNode.next) {
+                currentNode = currentNode.next
+            }
+            currentNode.next = newNode
+        } else {
+            this.head = newNode
+        }
+    }
+
 }
 
-const firstNode = new SinglyLinkedListNode(1)
-const secondNode = new SinglyLinkedListNode(2)
-const thirdNode = new SinglyLinkedListNode(3)
+// const firstNode = new SinglyLinkedListNode(1)
+// const secondNode = new SinglyLinkedListNode(2)
+// const thirdNode = new SinglyLinkedListNode(3)
 const llist = new SinglyLinkedList();
-llist.head = firstNode;
-firstNode.next = secondNode;
-secondNode.next = thirdNode;
+// llist.head = firstNode;
+// firstNode.next = secondNode;
+// secondNode.next = thirdNode;
+llist.insertNodeAtTail(1);
+llist.insertNodeAtTail(2);
+llist.insertNodeAtTail(3);
 
 llist.printllist()
